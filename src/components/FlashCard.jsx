fa33efe885a0271b9e32b7622dcc10fc3acd6fa6
henry paredes <henryalexander24@gmail.com>
2025-07-20 13:33:11 -0400

    separated functionality in three menus

--- src/components/FlashCard.jsx
+++ src/components/FlashCard.jsx
@@ -15,7 +15,7 @@ const Flashcard = ({ cardData, isFlipped, onFlip }) => {
     }
 
     return (
-        <div style={containerStyle} className="w-full h-64" onClick={onFlip}>
+        <div style={containerStyle} className="w-full h-62" onClick={onFlip}>
             <div style={cardStyle} className="relative w-full h-full transition-transform duration-700">
                 <div style={faceStyle} className="absolute w-full h-full bg-white rounded-xl shadow-lg flex justify-center items-center p-6">
                     <p className="text-3xl font-semibold text-gray-800 text-center">{cardData.spanish}</p>
