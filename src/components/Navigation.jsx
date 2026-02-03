22a8410c773601be6308f6d70e9d6938acd53dd9
henry paredes <henryalexander24@gmail.com>
2026-02-03 06:30:02 -0500

    navigation responsive

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { MdCreditCard } from 'react-icons/md';
 import { FaBookOpen, FaCalendarAlt } from 'react-icons/fa';
 import { IoPersonSharp } from 'react-icons/io5';
 import { PiCardsFill } from 'react-icons/pi';
 
 const Navigation = () => {
     return (
-        <nav className="fixed bottom-0 left-1/2 transform -translate-x-1/2 w-xl bg-white dark:bg-gray-800 border-t border-gray-200 dark:border-gray-700 shadow-[0_-4px_6px_-1px_rgba(0,0,0,0.1)] z-50">
-            <div className="max-w-2xl mx-auto flex justify-between items-center h-16">
+        <nav className="fixed bottom-0 left-0 w-full bg-white dark:bg-gray-800 border-t border-gray-200 dark:border-gray-700 shadow-[0_-4px_6px_-1px_rgba(0,0,0,0.1)] z-50">
+            <div className="max-w-2xl mx-auto flex justify-around items-center h-16">
                 <NavLink 
                     to="/speak" 
                     className={({ isActive }) => `flex flex-col items-center justify-center w-full h-full transition-colors duration-200 ${isActive ? 'text-teal-600 dark:text-teal-400' : 'text-gray-500 dark:text-gray-400 hover:text-teal-500 dark:hover:text-teal-300'}`}
